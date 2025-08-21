22c4ac438a0749341c9df64f04cf5195e6055ca9
Advenatrix <enders.sky@gmail.com>
2025-08-21 16:05:15 +0800

    userfix and qol

--- app/Filament/Widgets/TodoChart.php
+++ app/Filament/Widgets/TodoChart.php
@@ -4,21 +4,28 @@ namespace App\Filament\Widgets;
 
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Todo;
 
 class TodoChart extends StatsOverviewWidget
 {
     protected function getStats(): array
     {
+        $pending = Todo::where('status', 'pending')->count();
+        $inProgress = Todo::where('status', 'in_progress')->count();
+        $completed = Todo::where('status', 'completed')->count();
+
         return [
-            Stat::make('Pending Todos', '20')
-            ->description('7 increase')
-            ->descriptionIcon('heroicon-m-arrow-trending-up'),
-            Stat::make('In Progress', '32')
-            ->description('3 decrease')
-            ->descriptionIcon('heroicon-m-arrow-trending-down'),
-            Stat::make('Finished Todos', '13')
-            ->description('3 increase')
-            ->descriptionIcon('heroicon-m-arrow-trending-up'),
+            Stat::make('Pending Todos', $pending)
+                ->description('Live count from DB')
+                ->descriptionIcon('heroicon-m-arrow-trending-up'),
+
+            Stat::make('In Progress', $inProgress)
+                ->description('Live count from DB')
+                ->descriptionIcon('heroicon-m-arrow-trending-down'),
+
+            Stat::make('Finished Todos', $completed)
+                ->description('Live count from DB')
+                ->descriptionIcon('heroicon-m-arrow-trending-up'),
         ];
     }
 }
